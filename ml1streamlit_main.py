a15fb50267ff0d9868ab2a1a4ec0774c057bbee3
Agung Gunawan <59593824+insomniagung@users.noreply.github.com>
2023-01-09 23:39:33 +0700

    Update ml1streamlit_main.py

--- ml1streamlit_main.py
+++ ml1streamlit_main.py
@@ -4,11 +4,6 @@ import pandas as pd
 import numpy as np
 import pickle #untuk menyimpan atau membaca data .pkl
 import base64 #untuk membuka file .gif di streamlit
-from sklearn.preprocessing import LabelEncoder
-from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import accuracy_score
 
 # @st.cache adalah meningkatkan performa web
 @st.cache(suppress_st_warning = True)
